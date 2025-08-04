fb00b82c852172e368e87efeae59ab7519121820
jagath-sajjan <jagathsajjan097@gmail.com>
2025-08-04 19:13:10 +0530

    first

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://jogoblogs.vercel.app/", // replace this with your deployed domain
+  author: "Jagath Sajjan",
+  profile: "https://github.com/jagath-sajjan",
+  desc: "Jagath Sajjan's Blogs Are Here",
+  title: "JogoBlogs",
+  ogImage: "s.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Kolkata",
 } as const;
---
+++ src/data/blog/2025/first-c-program.md
@@ -0,0 +1,21 @@
+---
+author: "Jagath Sajjan"
+title: "First C Program Of Mine Lol"
+slug: "first-c-program"
+featured: true
+draft: false
+tags:
+  - "C-Lang"
+description: "Just My First Program In C Hello World"
+pubDatetime: 2025-08-04T19:00:00+05:30
+---
+
+# Hello World in C - First Program
+
+```c
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
